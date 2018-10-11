5b1fdc45eaa7ec23749d04fd4cb2390f426e97e7
STMaktab <zahra.setoodeh@gmail.com>
2018-10-11 16:20:15 +0330

    comments add

--- src/Client.java
+++ src/Client.java
@@ -5,9 +5,12 @@ public class Client
   public static void main(String[] args) throws Exception
   {
      Socket sock = new Socket("192.168.1.103", 8000);
+     // reading from keyboard 
      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+     // sending to server (write object)
      OutputStream ostream = sock.getOutputStream(); 
      PrintWriter pwrite = new PrintWriter(ostream, true);
+     // receiving from server ( receiveRead  object)
      InputStream istream = sock.getInputStream();
      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
@@ -16,8 +19,11 @@ public class Client
      String receiveMessage, sendMessage;               
      while(true)
      {
+    	 // reading input massage
         sendMessage = keyRead.readLine();  
-        pwrite.println(sendMessage);      
+        // sending to server
+        pwrite.println(sendMessage);    
+        // flush the data
         pwrite.flush();                 
         if((receiveMessage = receiveRead.readLine()) != null) 
         {
--- src/Server.java
+++ src/Server.java
@@ -6,10 +6,13 @@ public class Server
   {
       ServerSocket sersock = new ServerSocket(8000);
       System.out.println("Server ready for chatting");
-      Socket sock = sersock.accept( );              
+      Socket sock = sersock.accept( );                          
+      // reading from keyboard 
       BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	  // sending to client (write object)
       OutputStream ostream = sock.getOutputStream(); 
       PrintWriter pwrite = new PrintWriter(ostream, true);
+      // receiving from client ( receiveRead  object)
       InputStream istream = sock.getInputStream();
       BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
